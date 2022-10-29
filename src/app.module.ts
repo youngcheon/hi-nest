9c7cf0b1fa9b23cd50376a2946abf251712b7b1c
youngcheon <kimyoungcheon97@gmail.com>
2022-10-29 16:21:02 +0900

    2.6 modules and dependency injection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MoviesController } from './movies/movies.controller';
 import { MoviesService } from './movies/movies.service';
+import { MoviesModule } from './movies/movies.module';
+import { AppController } from './app.controller';
 
 @Module({
-  imports: [],
-  controllers: [MoviesController],
+  imports: [MoviesModule],
+  controllers: [MoviesController, AppController],
   providers: [MoviesService],
 })
 export class AppModule {}
--- src/movies/dto/create-movie.dto.ts
+++ src/movies/dto/create-movie.dto.ts
@@ -3,8 +3,10 @@ import { IsString, IsNumber, IsOptional } from 'class-validator';
 export class CreateMovieDto {
   @IsString()
   readonly title: string;
+
   @IsNumber()
   readonly year: number;
+
   @IsOptional()
   @IsString({ each: true })
   readonly genres: string[];
